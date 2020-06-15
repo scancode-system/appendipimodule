2362c54ddd88eb867be4cdae349e014afd3d04d9
Your Name <you@example.com>
2020-06-15 12:33:10 -0300

    .

--- Providers/AppendIpiServiceProvider.php
+++ Providers/AppendIpiServiceProvider.php
@@ -8,6 +8,16 @@ use Illuminate\Database\Eloquent\Factory;
 class AppendIpiServiceProvider extends ServiceProvider
 {
     /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'AppendIpi';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'appendipi';
+
+    /**
      * Boot the application events.
      *
      * @return void
@@ -35,11 +45,18 @@ class AppendIpiServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         $this->publishes([
+            module_path($this->moduleName, 'Config/config.php') => config_path($this->moduleNameLower . '.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path($this->moduleName, 'Config/config.php'), $this->moduleNameLower
+        );
+        
+        /*$this->publishes([
             __DIR__.'/../Config/config.php' => config_path('appendipi.php'),
         ], 'config');
         $this->mergeConfigFrom(
             __DIR__.'/../Config/config.php', 'appendipi'
-        );
+        );*/
     }
 
 
