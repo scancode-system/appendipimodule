8c3332fc546d0501f9de9b7828e3a5a9b75b9f88
Your Name <you@example.com>
2020-07-30 15:55:35 -0300

    .

---
+++ Services/InfoService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Modules\AppendIpi\Services; 
+
+
+use Modules\Portal\Services\Validation\Data\InfoValidationService;
+use Modules\Portal\Rules\NullRule;
+
+
+class InfoService extends InfoValidationService
+{
+
+
+	public function rule($data, $index, $columns)
+	{
+		return [
+			'ipi' =>  'numeric|between:0,100|regex:/^\d+(\.\d{1,2})?$/'
+		];
+	}
+
+	public function modifiers()
+	{
+		return  [
+			['rule' => ['ipi' => [new NullRule()]], 'filter' => 'setToZero']
+		];
+	}
+
+	public function columnsFormat()
+	{
+		return  [];
+	}
+
+}
